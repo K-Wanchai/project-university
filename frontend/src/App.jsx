92f02c2e468b31a7bd80042d38438d8e3bcea7ee
Sirichai <pop454.1112@gmail.com>
2026-04-24 11:53:26 +0700

    Money

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,8 @@ import SchoolInfo from "./pages/admin/SchoolInfo.jsx";
 import EditSchoolInfo from "./pages/admin/EditSchoolInfo.jsx";
 import Examination from "./pages/admin/Examination.jsx";
 import EditExamination from "./pages/admin/EditExamination.jsx";
-import AddExamination from "./pages/admin/AddExamination.jsx"; // Import ไฟล์ใหม่
+import AddExamination from "./pages/admin/AddExamination.jsx";
+import Money from "./pages/admin/Money.jsx"; 
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
         <Route path="/examination" element={<Examination />} />
         <Route path="/edit-examination/:id" element={<EditExamination />} />
         <Route path="/add-examination" element={<AddExamination />} />
+         <Route path="/money" element={<Money />} />
       </Routes>
     </Router>
   );
